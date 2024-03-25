fd6306a04eb15a44874741f20e4e230a27db8b8b
Christian <152685628+Yipper91@users.noreply.github.com>
2024-03-25 16:48:44 -0700

    Dropdown

--- js/nav.js
+++ js/nav.js
@@ -1,23 +1,31 @@
-let bgAnimation;
-const bg = document.querySelector(".bg");
-const bg_start = document.querySelector(".bg-start");
-const start_container = document.querySelector(".start-container");
-const num = document.querySelector(".score-container");
-const rings = document.querySelector(".ring-collection");
-const startBtn = document.querySelector(".start");
-const pauseShow = document.querySelector(".pausebtn");
+let counter = 1;
+const burger = document.querySelector("#burger");
+const dropNav = document.querySelector("#dropNav");
+const dropBg = document.querySelector("#dropBg");
 
-startBtn.addEventListener("click", startGame, false);
+burger.addEventListener("click", operate, false);
 
-function startGame() {
-    bg.style.animation = "moveBG 2s linear infinite";
-    num.style.animation = "score-move 15s linear infinite";
-    rings.style.animation = "rings 3s linear infinite";
-    bg_start.style.opacity = "0%";
-    start_container.style.opacity = "0%";
-    pauseShow.style.opacity = "100%";
-    sonic_container.style.opacity = "100%";
-    sonic_img.style.marginRight = "0vh";
-    sonic_gem.style.opacity = "0%";
-    startBtn.style.opacity = "0%";
+function operate() {
+    if (counter == 1) {
+        drop();
+        counter = 0;
+    } else if (counter == 0) {
+        rise();
+        counter = 1;
+    }
+}
+
+const drop = () => {
+    dropBg.style.visibility = "visible";
+    dropNav.style.animation = "drop 0.7s steps(22) forwards";}
+
+const rise = () => {
+    dropNav.style.animation = "rise 0.7s steps(22) forwards";
+    setTimeout(function() {
+        nav_hide();
+    }, 700);
+}
+
+function nav_hide () {
+    dropBg.style.visibility = "hidden";
 }
