0e8515d898d1805836149329579fa95094dd6409
Christian <152685628+Yipper91@users.noreply.github.com>
2024-04-09 05:55:32 +0000

    Plugins

---
+++ js/flickity.js
@@ -0,0 +1,35 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+    contain: true,
+    imagesLoaded: true,
+    fade: true,
+    wrapAround: true,
+    prevNextButtons: false,
+});
+var flkty = new Flickity( '.main-carousel', {
+    
+});
+
+var elem = document.querySelector('.main-carousel2');
+var flkty = new Flickity( elem, {
+    contain: true,
+    imagesLoaded: true,
+    fade: true,
+    wrapAround: true,
+    prevNextButtons: false,
+});
+var flkty = new Flickity( '.main-carousel2', {
+    
+});
+
+var elem = document.querySelector('.main-carousel3');
+var flkty = new Flickity( elem, {
+    contain: true,
+    imagesLoaded: true,
+    fade: true,
+    wrapAround: true,
+    prevNextButtons: false,
+});
+var flkty = new Flickity( '.main-carousel3', {
+    
+});
---
+++ js/scrollreveal.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal('#textrev');
+ScrollReveal().reveal('#footer1', { delay: 100 });
+ScrollReveal().reveal('#footer2', { delay: 300 });
+ScrollReveal().reveal('#footer3', { delay: 500 });
+ScrollReveal().reveal('#footer4', { delay: 700 });
+
+ScrollReveal().reveal('#li0');
+ScrollReveal().reveal('#li1', { delay: 300 });
+ScrollReveal().reveal('#li2', { delay: 600 });
+ScrollReveal().reveal('#li3', { delay: 900 });
